13112a04e5ed585659285d500df14259d5ab8bbe
yangkui <752544765@qq.com>
2018-12-26 23:00:23 +0800

    添加尺寸model

---
+++ src/main/java/cn/navigation/education/changliao/model/Size.java
@@ -0,0 +1,35 @@
+package cn.navigation.education.changliao.model;
+
+/**
+ *
+ * 尺寸model
+ *
+ */
+public class Size {
+    private double width;
+    private double height;
+
+    public Size(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Size() {
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+}
