f6e804ccaaa26f9160864a06d201a97a859c5a34
yangkui <752544765@qq.com>
2018-12-27 20:16:29 +0800

    fix bug

--- src/main/java/cn/navigation/education/changliao/base/NavigatorFXControl.java
+++ src/main/java/cn/navigation/education/changliao/base/NavigatorFXControl.java
@@ -2,6 +2,10 @@ package cn.navigation.education.changliao.base;
 
 import javafx.scene.control.Control;
 
+/**
+ * 自定义组件
+ */
+
 public abstract class NavigatorFXControl extends Control {
     private String styleSheet;
 
