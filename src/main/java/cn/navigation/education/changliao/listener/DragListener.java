5171282c7c276391f8dfad1e13e7473becc73033
yangkui <752544765@qq.com>
2019-01-01 00:34:17 +0800

    添加部分文字说明

--- src/main/java/cn/navigation/education/changliao/listener/DragListener.java
+++ src/main/java/cn/navigation/education/changliao/listener/DragListener.java
@@ -22,17 +22,26 @@ public class DragListener implements EventHandler<MouseEvent> {
 
     @Override
     public void handle(MouseEvent event) {
+
         event.consume();
+
         if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+
             xOffset = event.getSceneX();
             yOffset = event.getSceneY();
+
         } else if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+            /**
+             * 主意此处 api中有规定 正值向右移动，负值想左移动
+             */
             stage.setX(event.getScreenX() - xOffset);
+
             if (event.getScreenY() - yOffset < 0) {
                 stage.setY(0);
             } else {
                 stage.setY(event.getScreenY() - yOffset);
             }
+
         }
     }
 
